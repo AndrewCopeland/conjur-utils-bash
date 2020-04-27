259b1304e11313316851a40e73567ff8faf22e04
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2020-04-27 14:34:52 -0400

    Add files via upload

---
+++ conjur_utils.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+util_defaults="set -u"
+
+function conjur_verbose {
+  if [ "${CONJUR_VERBOSE}" = "true" ]; then
+    echo "-v"
+  fi
+}
+verbose=$(conjur_verbose)
+
+# this will return the authorization header used for api calls for other methods
+function conjur_authenticate {
+	$util_defaults
+    api_key=$(curl $verbose --fail -s -k --user "$CONJUR_AUTHN_LOGIN:$CONJUR_AUTHN_API_KEY" $CONJUR_APPLIANCE_URL/authn/$CONJUR_ACCOUNT/login)
+	session_token=$(curl $verbose --fail -s -k --data "$api_key" $CONJUR_APPLIANCE_URL/authn/$CONJUR_ACCOUNT/admin/authenticate)
+	token=$(echo -n $session_token | base64 | tr -d '\r\n')
+	header="Authorization: Token token=\"$token\""
+	echo "$header"
+}
+
+function conjur_configure_authn_iam {
+	$util_defaults
+	service_id=$1
+	policy_branch="root"
+	mkdir -p "./tmp"
+	file_name="./tmp/authn-iam-$service_id.yml"
+	sed "s/\<service-id\>/$service_id/g" "authenticators/authn-iam.yml" > $file_name
+	conjur_append_policy "${policy_branch}" "${file_name}"
+}
+
+function conjur_append_policy {
+	$util_defaults
+	policy_branch=$1
+	policy_name=$2
+	header=$(conjur_authenticate)
+	response=$(curl -H "$header" -X POST -d "$(< $policy_name)" -s -k $CONJUR_APPLIANCE_URL/policies/$CONJUR_ACCOUNT/policy/$policy_branch)
+	echo "$response"
+}
+
+function conjur_set_variable {
+	$util_defaults
+	variable_name=$1
+	variable_value=$2
+	header=$(conjur_authenticate)
+	curl -k -s -H "$header" --data "$variable_value" "$CONJUR_APPLIANCE_URL/secrets/$CONJUR_ACCOUNT/variable/$variable_name"
+}
+
+function conjur_get_variable {
+	$util_defaults
+	variable_name=$1
+	header=$(conjur_authenticate)
+	value=$(curl -k -s -H "$header" "$CONJUR_APPLIANCE_URL/secrets/$CONJUR_ACCOUNT/variable/$variable_name")
+	echo "${value}"
+}
+
+function conjur_resources {
+  	$util_defaults
+	header=$(conjur_authenticate)
+	curl -k -s -H "$header" "$CONJUR_APPLIANCE_URL/resources/$CONJUR_ACCOUNT" | jq
+}
+
+function conjur_list {
+	$util_defaults
+	resources=$(conjur_resources)
+	echo "${resources}" | jq -r .[].id
+}
